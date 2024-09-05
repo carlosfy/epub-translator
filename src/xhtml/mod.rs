5c567f18fa3b0a54b929e7bd692c3e2ce01cf8cd
Carlos Yago <carlosfyago@gmail.com>
2024-09-05 17:15:23 +0200

    Fix XHTML module
    Add Regex lib.
    Treat string before parsing to remove self-closing tags.
    Process string before writing to file to correct:
    - Self-closing tags
    - Empty span tags
    - Non-breaking spaces
    
    See BUGS.md, items 2 and 3.

--- src/xhtml/mod.rs
+++ src/xhtml/mod.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::fs::File;
 use std::io::{Read, Write};
 
@@ -14,6 +15,10 @@ use markup5ever_rcdom::{Node, NodeData, RcDom};
 
 // Parses a string containing XHTML and returns the document node.
 pub fn get_document_node(content: &str) -> Result<Rc<Node>, Box<dyn std::error::Error>> {
+    // remove self closing span tags <span*/> => <span*></span>
+    let re = Regex::new(r"<span([^>]*?)/>")?;
+    let content = re.replace_all(&content, "<span$1></span>");
+
     let rc_dom = parse_document(RcDom::default(), Default::default())
         .from_utf8()
         .read_from(&mut content.as_bytes())?;
@@ -47,6 +52,17 @@ pub fn serialize_document(
     document: &Rc<Node>,
     output_path: &PathBuf,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    let output_string = serialize_document_to_string(document)?;
+
+    let mut file = File::create(output_path)?;
+    file.write_all(output_string.as_bytes())?;
+
+    Ok(())
+}
+
+pub fn serialize_document_to_string(
+    document: &Rc<Node>,
+) -> Result<String, Box<dyn std::error::Error>> {
     let opts = SerializeOpts {
         traversal_scope: TraversalScope::IncludeNode,
         ..Default::default()
@@ -60,10 +76,24 @@ pub fn serialize_document(
         serialize(&mut output, &serializable, opts.clone())?;
     }
 
-    let mut file = File::create(output_path)?;
-    file.write_all(&output)?;
+    let mut output_string = String::from_utf8(output)?;
 
-    Ok(())
+    // Change `>` to `/>` in self closing tags
+    let self_closing_tags = ["br", "hr", "img", "input", "link", "meta"];
+    for tag in self_closing_tags.iter() {
+        let re = Regex::new(&format!(r"(<{}[^>]*?)>", tag).to_string())?;
+        output_string = re.replace_all(&output_string, "$1/>").to_string();
+    }
+
+    // Todo: test
+    let re_nbsp = Regex::new(r"&nbsp;")?;
+    output_string = re_nbsp.replace_all(&output_string, "\u{00A0}").to_string();
+
+    // Todo replace empty spans by selfclosing spans?
+    let re_span = Regex::new(r"<span([^>]*?)></span>")?;
+    output_string = re_span.replace_all(&output_string, "<span$1/>").to_string();
+
+    Ok(output_string)
 }
 
 pub fn get_document_node_from_path(
@@ -94,8 +124,10 @@ mod tests {
 
     #[test]
     fn test_modify_text_nodes() -> Result<(), Box<dyn std::error::Error>> {
+        let temp_dir = tempfile::tempdir()?;
+
         let input_file = "tests/data/lorem.xhtml";
-        let output_file = "temp/lorem_processed1.xhtml";
+        let output_file = temp_dir.path().join("lorem_processed.xhtml");
         let expected_file = "tests/data/lorem_expected.xhtml";
 
         // Create the root node of the DOM.
@@ -115,12 +147,42 @@ mod tests {
         }
 
         // Serialize the DOM
-        serialize_document(&document, &PathBuf::from(output_file))?;
+        serialize_document(&document, &output_file)?;
 
         let processed_content = fs::read_to_string(output_file)?;
         let expected_content = fs::read_to_string(expected_file)?;
 
         assert_eq!(processed_content, expected_content);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_serialize_and_deserialize() -> Result<(), Box<dyn std::error::Error>> {
+        let input_xhtml = r#"
+            <html>
+                <head>
+                </head>
+                <body>
+                    <figure class="figure-class" id="img-ge1">
+                        <span epub:type="pagebreak" id="pg5"/>
+                        <img alt="ima" id="im01" src="../images/pg01.jpg">
+                        <figcaption id="fig01">Figure caption.</figcaption>
+                    </figure>
+                </body>
+            </html>
+        "#;
+
+        let expexted_xhtml = "<html><head>\n                </head>\n                <body>\n                    <figure class=\"figure-class\" id=\"img-ge1\">\n                        <span epub:type=\"pagebreak\" id=\"pg5\"/>\n                        <img alt=\"ima\" id=\"im01\" src=\"../images/pg01.jpg\">\n                        <figcaption id=\"fig01\">Figure caption.</figcaption>\n                    </figure>\n                \n            \n        </body></html>";
+
+        let document = get_document_node(input_xhtml)?;
+        let processed_input_xhtml = serialize_document_to_string(&document)?;
+
+        // write to file in /tests/data/test_serialize_and_deserialize.xhtml
+        let mut file = fs::File::create("tests/data/test_serialize_and_deserialize.xhtml")?;
+        file.write_all(processed_input_xhtml.as_bytes())?;
+
+        assert_eq!(processed_input_xhtml, expexted_xhtml);
 
         Ok(())
     }
